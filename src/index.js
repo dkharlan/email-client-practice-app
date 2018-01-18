4b3f0457a028103b9fbaf1b9bc3ff9680c7eda23
Dave Harlan <dkharlan@umich.edu>
2018-01-18 10:16:01 -0500

    Render some templates and stuff

--- src/index.js
+++ src/index.js
@@ -2,26 +2,88 @@
  * @flow
  */
 
-// example of generating HTML with js
-function renderSidebar() {
-  const sidebarContents = `
-    <h2 class="email-header">Inbox</h2>
-    <ul class="email-list">
-      <li>
-        <button class="email-item" type="button">
-          <div class="sender-details">
-            <p>Email sender</p>
-            <span>Timestamp</span>
-          </div>
-          <p class="email-subject">Email1 subject</p>
-          <p>Email snippet</p>
-        </button>
-      </li>
-    </ul>
-  `;
+import type { Message, MessageKey } from './store/types';
+import store from './store/index';
 
-  const container = document.querySelector('.email-list-container');
-  if (container != null) container.innerHTML = sidebarContents;
+type EmailDetails = {
+  sender: string,
+  timestamp: string, // TODO for now
+  subject: string,
+  snippet: string
 }
 
-renderSidebar();
+type MessageEntry = {[id: MessageKey]: Message};
+
+function emailToDetails(email: Message) {
+  const headers = email.payload.headers;
+
+  const fromHeader = headers.find((h) => h.name === 'From');
+  const subjectHeader = headers.find((h) => h.name === 'Subject');
+
+  if(!fromHeader) {
+    throw 'Email has no "From" header';
+  }
+  if(!subjectHeader) {
+    throw 'Email has no "Subject" header';
+  }
+
+  return {
+    sender: fromHeader.value,
+    timestamp: email.internalDate,
+    subject: subjectHeader.value,
+    snippet: email.snippet
+  }
+}
+
+function emailDetailsTemplate(details: EmailDetails) {
+  return `
+    <li>
+      <button class="email-item" type="button">
+        <div class="sender-details">
+          <p>${details.sender}</p>
+          <span>${details.timestamp}</span>
+        </div>
+        <p class="email-subject">${details.subject}</p>
+        <p>${details.snippet}</p>
+      </button>
+    </li>
+  `;
+}
+
+// TODO needs types. better yet, replace with 'compose' from some lib
+const emailToTemplate = (e) => emailDetailsTemplate(emailToDetails(e));
+
+// TODO needs some types, see if this can be replaced by something from a lib
+function byTime(a, b) {
+  if(a > b) {
+    return -1;
+  }
+  else if (a < b) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+function sidebarTemplate(emailEntries: MessageEntry) {
+  const emails = Object.values(emailEntries).sort(byTime);
+  const emailsTemplateFragment = emails.reduce((accum, e) => accum + '\n' + emailToTemplate(e), '');
+  return `
+      <h2 class="email-header">Inbox</h2>
+      <ul class="email-list">
+        ${emailsTemplateFragment}
+      </ul>
+  `;
+}
+
+function render(selector, template) {
+  const element = document.querySelector(selector);
+  if(element) {
+    element.innerHTML = template;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  render('.email-list-container', sidebarTemplate(store.messages));
+});
