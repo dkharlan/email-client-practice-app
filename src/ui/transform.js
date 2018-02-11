1c6266ab3093b82137c574b6fc2c5dc1e9f03640
Dave Harlan <dkharlan@umich.edu>
2018-02-11 16:29:22 -0500

    Allow emails with no subject

--- src/ui/transform.js
+++ src/ui/transform.js
@@ -5,22 +5,18 @@ import { unescape } from '../utility/misc';
 // TODO don't mix validation and data munging
 export const messageToDetails = (email: Message) => {
   const headers = email.payload.headers;
-
   const fromHeader = headers.find((h) => h.name === 'From');
   const subjectHeader = headers.find((h) => h.name === 'Subject');
 
   if(!fromHeader) {
     throw 'Email has no "From" header';
   }
-  if(!subjectHeader) {
-    throw 'Email has no "Subject" header';
-  }
 
   return {
     id:      email.id,
     sender:  unescape(fromHeader.value),
     date:    moment(parseInt(email.internalDate)),
-    subject: subjectHeader.value,
+    subject: subjectHeader ? subjectHeader.value : '(No subject)',
     snippet: email.snippet
   }
 };
