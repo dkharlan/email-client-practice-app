d830bcb8d1ac4fa764594dc9d29afc8fa4a4ceed
Dave Harlan <dkharlan@umich.edu>
2018-02-11 18:09:14 -0500

    Tweak types

--- src/ui/types.js
+++ src/ui/types.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import type { Message, MessageKey, ThreadKey, Label } from '../store/types';
+import type { Message, MessageKey, Label } from '../store/types';
 
 // TODO merge these with store/types?
 
@@ -14,12 +14,7 @@ export type MessageDetails = {
 
 export type MessageEntry = {[id: MessageKey]: Message};
 
-export type ThreadDetails = {
-  id: ThreadKey,
-  messages: Array<MessageDetails>
-};
-
 export type MailboxDetails = {
   name: Label,
-  threads: Array<ThreadDetails>
+  threads: Array<MessageDetails>
 };
